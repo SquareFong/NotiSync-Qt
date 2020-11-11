d31da33d81a70350dd1cbd6817bcf1a6854ab8fe
Square Fong <squarefong17@gmail.com>
2020-11-12 04:34:03 +0800

    add auto mkdir

--- src/core/configsmanager.cpp
+++ src/core/configsmanager.cpp
@@ -8,6 +8,11 @@ ConfigsManager::ConfigsManager()
     , serverCfgName("server.json")
     , clientCfgName("NotiSyncQt.json")
 {
-    if (opendir(path.c_str()) == 0) //如果文件夹不存在
-        mkdir(path.c_str(), 0660);
+    if (opendir(path.c_str()) == NULL) //如果文件夹不存在
+    {
+        int ret = mkdir(path.c_str(), 0660);
+        if (ret != 0) {
+            throw "Fail to local config files";
+        }
+    }
 }
