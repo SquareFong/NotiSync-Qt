6562317454b0ba85b3ce8f83fcbf98e707fa39a1
Square Fong <squarefong17@gmail.com>
2020-11-20 04:21:54 +0800

    change the time style of digest, and auto select to show Number ro Name in title

--- src/widgets/messagesitemdelegate.cpp
+++ src/widgets/messagesitemdelegate.cpp
@@ -2,10 +2,33 @@
 
 #include <QPainter>
 #include <QPainterPath>
+#include <string>
+using namespace std;
 
 MessagesItemDelegate::MessagesItemDelegate(QObject* parent)
     : QStyledItemDelegate(parent)
 {
+}
+
+string strTimeFormat(const string& ti)
+{
+    time_t t = stol(ti) / 1000;
+    time_t now;
+    time(&now);
+    struct tm* timeinfo;
+    timeinfo = localtime(&t);
+    //小于一天
+    char buffer[100];
+    if (now - t < 86400) {
+        strftime(buffer, 100, "%H:%M", timeinfo);
+    } else if (now - t < 604800) { //小于一周
+        strftime(buffer, 100, "%A", timeinfo);
+    } else if (now - t < 31536000) { //小于一年
+        strftime(buffer, 100, "%m.%d", timeinfo);
+    } else {
+        strftime(buffer, 100, "%Y", timeinfo);
+    }
+    return string(buffer);
 }
 
 void MessagesItemDelegate::paint(QPainter* painter,
@@ -56,17 +79,17 @@ void MessagesItemDelegate::paint(QPainter* painter,
         painter->drawImage(iconRect, QImage(itemData.IconPath));
 
         QFontMetrics fm = painter->fontMetrics();
-        QString content = fm.elidedText(itemData.Contact,
+        QString contact = fm.elidedText((itemData.Contact.size() == 0 ? itemData.Number : itemData.Contact),
             Qt::ElideRight,
             titleRect.width(),
             Qt::TextShowMnemonic);
         painter->setPen(QPen(Qt::black));
         painter->setFont(QFont("Noto Mono", 13));
-        painter->drawText(titleRect, content);
+        painter->drawText(titleRect, contact);
 
         painter->setPen(QPen(Qt::gray));
         painter->setFont(QFont("Noto Mono", 13));
-        painter->drawText(timeRect, itemData.Time);
+        painter->drawText(timeRect, strTimeFormat(itemData.Time.toStdString()).c_str());
 
         QFontMetrics fm1 = painter->fontMetrics();
         QString content1 = fm1.elidedText(itemData.Content,
